72dc63c169c39f9dc4f7361fd5dd891cc931e361
Daniel Macauyag <dmacauyag92@gmail.com>
2018-03-06 22:02:50 -0800

    add styles, remove unused import

--- src/main/javascript/components/VenueListDetail/VenueListDetail.js
+++ src/main/javascript/components/VenueListDetail/VenueListDetail.js
@@ -1,4 +1,4 @@
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
+import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import React from 'react'
